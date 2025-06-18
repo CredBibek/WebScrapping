c50558b1ea0094f4982072ece8486d70c46c7440
CredBibek <bibek.das@credresolve.com>
2025-06-18 15:07:44 +0530

    EDfcgchvjb

--- src/test/java/org/example/SumTest.java
+++ src/test/java/org/example/SumTest.java
@@ -27,4 +27,48 @@ public class SumTest {
         // Assert
         assertEquals(expectedOutput, outputStream.toString());
     }
+
+    @Test
+    public void testMainMethodWithDefaultArray() {
+        // Arrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        String expectedOutput = "The sum is: 15" + System.lineSeparator();
+
+        // Act
+        Sum.main(new String[]{});
+
+        // Assert
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void testMainMethodWithModifiedInput() {
+        // Arrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(new java.io.ByteArrayInputStream("6".getBytes()));
+        String expectedOutput = "The sum is: 20" + System.lineSeparator();
+
+        // Act
+        Sum.main(new String[]{});
+
+        // Assert
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    public void testMainMethodWithEmptyInput() {
+        // Arrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(new java.io.ByteArrayInputStream("".getBytes()));
+        String expectedOutput = "The sum is: 60" + System.lineSeparator();
+
+        // Act
+        Sum.main(new String[]{});
+
+        // Assert
+        assertEquals(expectedOutput, outputStream.toString());
+    }
 }
